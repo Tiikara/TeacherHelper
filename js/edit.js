991ffd368ca2f98bebdf48adf4397e5566dc574f
Tikara <tiikara93@gmail.com>
2015-03-23 16:21:46 +1000

    Отображение спиннера во время удаления

--- js/edit.js
+++ js/edit.js
@@ -4,12 +4,20 @@ function updateEditEvents()
     $( ".delete-href" ).click(function(event) {
 
         event.preventDefault();
-        var itemId = $(this).attr("data-item-id");
-        var url = $(this).attr("href");
+
+        var webElement = $(this);
+
+        var itemId = webElement.attr("data-item-id");
+        var url = webElement.attr("href");
 
         modal_confirm({
             text: "Вы уверены, что хотите удалить элемент?",
             confirm: function() {
+
+                webElement.replaceWith('<div class="spinner"> \
+                                            <div class="spinner-icon"></div> \
+                                        </div>');
+
                 $.ajax({
                     url: url,
                     type: 'POST',
